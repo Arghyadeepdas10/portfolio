3de3157a898aa327f18de17e565e06a7b80df7eb
Arghyadeepdas10 <arghyadeepdas10@gmail.com>
2025-02-15 20:13:07 +0530

    add cv

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -70,7 +70,7 @@ const Portfolio = () => {
                             Ability to translate business requirements into technical solutions.
                             Ability to effectively self-manage during independent projects, as well as collaborate as part of a productive team.
                             </p>
-                            {/* <a href="#0" className="btn btn--medium u-fullwidth" >Download CV</a> */}
+                            <a href="https://drive.google.com/file/d/1ydQS19enOo9olIUdI4gieCltxuiapxcw/view" target='blank' className="btn btn--medium u-fullwidth" >Download CV</a>
                         </div>
                     </div>
                 </div> 
@@ -249,30 +249,11 @@ const Portfolio = () => {
                                 <a className="folio-list__proj-link" title="project link">
                                     <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
                                 </a>
-                            </li> 
-
-                            {/* <li className="folio-list__item column" >
-                                <a className="folio-list__item-link" href="#modal-06">
-                                    <div className="folio-list__item-pic">
-                                        <img src="images/portfolio/minimalismo.jpg" 
-                                             srcSet="images/portfolio/minimalismo.jpg 1x, images/portfolio/minimalismo@2x.jpg 2x" alt=""/>
-                                    </div>
-                                    
-                                    <div className="folio-list__item-text">
-                                        <div className="folio-list__item-cat">
-                                            Branding
-                                        </div>
-                                        <div className="folio-list__item-title">
-                                            Minimalismo.
-                                        </div>
-                                    </div>
-                                </a>
-                                <a className="folio-list__proj-link" href="#" title="project link">
-                                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
-                                </a>
-                            </li>  */}
+                            </li>                         
                         </ul> 
                     </div> 
+
+
                     {/* <div id="modal-01" hidden>
                         <div className="modal-popup">
                             <img src="images/portfolio/gallery/g-fuji.jpg" alt=""/>
@@ -289,7 +270,6 @@ const Portfolio = () => {
                             <a href="https://www.behance.net/" className="modal-popup__details">Project link</a>
                         </div>
                     </div> 
-
                     <div id="modal-02" hidden>
                         <div className="modal-popup">
                             <img src="images/portfolio/gallery/g-lamp.jpg" alt=""/>
@@ -305,7 +285,6 @@ const Portfolio = () => {
                             <a href="https://www.behance.net/" className="modal-popup__details">Project link</a>
                         </div>
                     </div> 
-
                     <div id="modal-03" hidden>
                         <div className="modal-popup">
                             <img src="images/portfolio/gallery/g-rucksack.jpg" alt=""/>
@@ -336,7 +315,6 @@ const Portfolio = () => {
                             <a href="https://www.behance.net/" className="modal-popup__details">Project link</a>
                         </div>
                     </div> 
-
                     <div id="modal-05" hidden>
                         <div className="modal-popup">
 
@@ -353,7 +331,6 @@ const Portfolio = () => {
                             <a href="https://www.behance.net/" className="modal-popup__details">Project link</a>
                         </div>
                     </div> 
-
                     <div id="modal-06" hidden>
                         <div className="modal-popup">
                             <img src="images/portfolio/gallery/g-minimalismo.jpg" alt=""/>
@@ -440,6 +417,9 @@ const Portfolio = () => {
                             </div> 
                         </div> 
                     </div> 
+
+
+           
             </section> 
 
             <section id="contact" className="s-contact target-section">
